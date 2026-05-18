e13ee7ec9c7ccde7eee4b919b8819a0483d17bc3
有住匠真 <arizumi.shoma@kyoei.co.jp>
2026-05-18 13:46:38 +0900

    Practice2-02arizumi

--- practice/src/main/java/jp/co/sss/practice/p02/q02/controller/Practice0202Controller.java
+++ practice/src/main/java/jp/co/sss/practice/p02/q02/controller/Practice0202Controller.java
@@ -1,5 +1,35 @@
 package jp.co.sss.practice.p02.q02.controller;
 
-public class Practice0202Controller {
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
-}
+@Controller
+public class Practice0202Controller {
+	// 3章 ソースコード 3-1
+		@RequestMapping("/show/redirect_page")
+		public String before() {
+			return "practice02/02/redirect_link" ;
+		}
+
+		// 3章 ソースコード 3-1
+		@RequestMapping("/absolute")
+		public String after() {
+			return "redirect:https://www.google.co.jp";
+		}
+
+		// 3章 ソースコード 3-4
+		@RequestMapping("/relative")
+		public String sampleTransition() {
+			return "redirect:/result";
+		}
+
+		// 3章 ソースコード 3-4
+		@RequestMapping("/result")
+		public String indexForward() {
+			return "practice02/02/redirect_result";
+		}
+
+
+		}
+
+
