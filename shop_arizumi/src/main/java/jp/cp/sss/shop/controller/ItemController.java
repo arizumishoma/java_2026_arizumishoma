dc2f312fd0023c9263fae21fc3ee913ec832ab0a
有住匠真 <arizumi.shoma@kyoei.co.jp>
2026-05-21 13:02:26 +0900

    Practice5-01 データベース操作の Spring 環境設定とデータベースの準備 アリズミ

---
+++ shop_arizumi/src/main/java/jp/cp/sss/shop/controller/ItemController.java
@@ -0,0 +1,20 @@
+package jp.cp.sss.shop.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import repository.ItemRepository;
+
+@Controller
+public class ItemController {
+	@Autowired
+	ItemRepository repository;
+	
+	@RequestMapping("/items/findAll")
+	public String showItemList(Model model) {
+		model.addAttribute("items", repository.findAll());
+		return "items/item_list";
+	}
+}
